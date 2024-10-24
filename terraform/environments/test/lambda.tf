51eb5b68a35a2b1f5798f8d8bfd1590c7f417548
kekkokers <root@xopowo.org>
2024-10-24 08:10:32 +0300

    Rename src

--- terraform/environments/test/lambda.tf
+++ terraform/environments/test/lambda.tf
@@ -1,8 +1,7 @@
 module "lambda" {
   source = "../../modules/aws-lambda"
 
-
-  lambda_source_file_path = "../../../src/main.py"
+  lambda_source_file_path = "../../../lambda/main.py"
   lambda_iam_role_arn     = module.iam.role_arn
   lambda_function_env_variables = {
     "S3_BUCKET_NAME" = "s3-exmaple-lambda-test-foo-bar"
---
+++ terraform/modules/aws-vpc/output.tf
@@ -0,0 +1,3 @@
+output "all" {
+  value = data.aws_vpc.this
+}
